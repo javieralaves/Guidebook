e7faf60f1f68a90becc7c99bc712b37ba97100b6
Javier Alaves <javieralaves@me.com>
2023-06-27 09:32:37 +0200

    App updates

---
+++ Guidebook/AttractionRow.swift
@@ -0,0 +1,20 @@
+//
+//  AttractionRow.swift
+//  Guidebook
+//
+//  Created by Javier Alaves on 26/6/23.
+//
+
+import SwiftUI
+
+struct AttractionRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AttractionRow_Previews: PreviewProvider {
+    static var previews: some View {
+        AttractionRow()
+    }
+}
---
+++ Guidebook/CityCard.swift
@@ -0,0 +1,20 @@
+//
+//  CityCard.swift
+//  Guidebook
+//
+//  Created by Javier Alaves on 26/6/23.
+//
+
+import SwiftUI
+
+struct CityCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CityCard_Previews: PreviewProvider {
+    static var previews: some View {
+        CityCard()
+    }
+}
